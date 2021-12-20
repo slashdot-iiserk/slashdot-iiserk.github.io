71fe439480a8a3e3bc147f9764495634959a6f29
shogun223 <82089482+shogun223@users.noreply.github.com>
2021-12-20 11:59:57 +0530

    events page

---
+++ js/eventDetails.js
@@ -0,0 +1,32 @@
+const pastEvents = [
+    {
+    name: "WEBSITE DEVELOPMENT WORKSHOP",
+    image: "./utils/images/pp.jpg",
+    description: "Website ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea     commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea "
+    },
+    {
+        name: "BLENDER WORKSHOP",
+        image: "./utils/images/yellow_circle.svg",
+        description: "Blender ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea     commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea"
+    },
+    {
+        name: "FIGMA WORKSHOP",
+        image: "./utils/images/red_circle.svg",
+        description: "Figma ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea     commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea"
+    }
+];
+
+const upcomingEvents = [
+    {
+        name: "Slashdot - Alumni Talk",
+        date: "17th Jan 2022",
+        venue: "Google Meet"
+    },
+    {
+        name: "Cyber security and cyber crime awareness session",
+        date: "18th Jan 2022",
+        venue: "Zoom"
+    }
+];
+
+export {pastEvents,upcomingEvents};
