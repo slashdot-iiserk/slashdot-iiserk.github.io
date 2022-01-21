845d74317171d5c6376624b36deeb84387aee909
AK6263 <abhaykshirsagar01@gmail.com>
2022-01-21 14:02:12 +0530

    added images

--- js/memberData.js
+++ js/memberData.js
@@ -1,16 +1,16 @@
 export const OBMembers = [
   {
     name: "Abhay Kshirsagar",
-    image: "../utils/images/img.png",
-    designation: "Treasurer",
+    image: "../assets/dp_3 - Kshirsagar Abhay Shrikrishna.jpg",
+    designation: "Convener",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit repellendus, ducimus in ex soluta vero, quo blanditiis accusantium fuga tenetur sed recusandae, debitis nisi. At ea soluta corporis enim molestiae consequatur cumque dolorum rem aperiam praesentium totam recusandae voluptate dolor exercitationem animi, expedita, aut dignissimos nihil illo ratione quisquam officiis?",
+      "I am Third Year BSMS student and the current Convener of SlashDot. My main motive for joining SlashDot was Club Money ... JK. I am interested in Computational Biology and Machine Learning and the club naturally resonated with my interests. Apart from begin the office bearer I also conduct Programming Workshops for the club. You can connect with by clicking the following links",
     socialLinks: {
-      facebook: "https://www.facebook.com/",
-      instagram: "https://www.instagram.com/",
-      linkedin: "https://www.linkedin.com/",
-      github: "https://www.github.com/",
-      twitter: "https://www.twitter.com/",
+      facebook: "https://ak6263.github.io/",
+      // instagram: "https://www.instagram.com/",
+      linkedin: "https://www.linkedin.com/in/abhay-kshirsagar-2bn2b/",
+      github: "https://www.github.com/ak6263",
+      twitter: "https://twitter.com/abhayK6264",
     },
   },
   {
