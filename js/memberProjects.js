622e64ab4bd8c47fa5558f9fe7a6b8508848e989
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-02-12 22:58:24 +0530

    Formatting changes

--- js/memberProjects.js
+++ js/memberProjects.js
@@ -1,17 +1,17 @@
 export const Projects = [
-    {
+  {
     name: "Random member A",
     title: "Project A",
     image: "./utils/images/projects/project_a.jpg",
-    },
-    {
+  },
+  {
     name: "Random member B",
     title: "Project b",
     image: "./utils/images/projects/project_b.png",
-    },
-    {
-        name: "Random member C",
+  },
+  {
+    name: "Random member C",
     title: "Project C",
     image: "./utils/images/projects/project_c.jpg",
-    }
+  },
 ];
