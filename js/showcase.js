ced4203e8e92577bccbb065c23241f875d489c9e
AK6263 <abhaykshirsagar01@gmail.com>
2022-03-10 01:54:50 +0530

    Add : Animations to Showcase Page

--- js/showcase.js
+++ js/showcase.js
@@ -1,9 +1,9 @@
 import { Projects } from "./showcaseData.js";
 
-function createCard(root, projectInfo) {
+function createCard(root, projectInfo, wow_time_delay) {
   const { projectName, image, desc, author } = projectInfo;
   root.innerHTML += `
-        <div class="card">
+        <div class="card animate__animated wow animate__fadeInUp animate__fast" data-wow-delay="${wow_time_delay}">
             <img class="projectImg" src="${image}" alt="">
             <h1 class="projectName">${projectName.toUpperCase()}</h1>
             <p class="projectDesc">${desc}</p>
@@ -13,9 +13,10 @@ function createCard(root, projectInfo) {
 
 function showcaseProjects(divName) {
   let row = document.getElementById(divName);
+  let wow_time_delay = 1.;
   for (let index = 0; index < Projects.length; index++) {
     var proj = Projects[index];
-    createCard(row, proj);
+    createCard(row, proj, (index < 2)?'0.5s':'0s');
   }
 }
 
