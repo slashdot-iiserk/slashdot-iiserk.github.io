b8ba94883ffa79ada607f31c31f67dcaae82b5c2
AK6263 <abhaykshirsagar01@gmail.com>
2022-03-13 14:00:16 +0530

    Added Sticky NavBar Control to Showcase

--- js/showcase.js
+++ js/showcase.js
@@ -3,7 +3,7 @@ import { Projects } from "./showcaseData.js";
 function createCard(root, projectInfo, wow_time_delay) {
   const { projectName, image, desc, author } = projectInfo;
   root.innerHTML += `
-        <div class="card animate__animated wow animate__fadeInUp animate__fast" data-wow-delay="${wow_time_delay}">
+        <div class="card animate__animated wow animate__fadeInUp animate__fast" data-wow-delay="${wow_time_delay}" data-wow-offset="-200">
             <img class="projectImg" src="${image}" alt="">
             <h1 class="projectName">${projectName.toUpperCase()}</h1>
             <p class="projectDesc">${desc}</p>
