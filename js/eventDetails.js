b1e8048fe97237d942a61b50b5abaabdd69dafde
AK6263 <abhaykshirsagar01@gmail.com>
2022-07-04 13:54:02 +0530

    added event details

--- js/eventDetails.js
+++ js/eventDetails.js
@@ -2,56 +2,69 @@ const pastEvents = [
   {
     name: "WEBSITE DEVELOPMENT WORKSHOP",
     image: "./utils/images/events/1.png",
-    description:
-      "Website ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea     commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea ",
+    description: 
+      "Presenting before you, the first ever milestone achieved by this club - the maiden workshop conducted, on Web Development skills. The audience spent an evening, surfing over the fundamentals of Git, GitHub and HTML Basics."
   },
   {
-    name: "BLENDER WORKSHOP",
+    name: "BEYOND ACADEMIA",
     image: "./utils/images/events/2.png",
     description:
-      "Blender ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea     commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea",
+      "Beyond Academia is a collaboration between Slashdot Coding and Design Club and IISER Kolkata Students' Alumni Cell, aiming to make you cognizant of the emerging computational tools used in scientific research.<br>In the very first episode of this series, to talk about Machine Learning, Data Science and Logics, we had with us, Dr. Sunipa Dev, an alumnus from 11MS DMS."
   },
   {
-    name: "FIGMA WORKSHOP",
+    name: "BLENDER WORKSHOP PART 1",
     image: "./utils/images/events/3.png",
     description:
-      "Figma ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea     commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea",
+      "The third workshop, presented by Pedisetti Venkatesh of 18MS, marked the inauguration of the Part 1 of Blender Basics, “Design Your Waifu” where we designed the ring that you would give to your waifu when she comes to laifu."
   },  
   {
-    name: "UNITY WORKSHOP",
+    name: "BLENDER WORKSHOP PART 2",
     image: "./utils/images/events/4.png",
     description:
-      "Unity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Unity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Unity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Unity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Unity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiUnity ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi"
+      "Ladies and gentlemen, Part 2 of Design Your Waifu is here! Last time, we modelled and textured the ring that you'll be giving to your waifu when you finally create her. But how will you talk to her every day when she's all the way in Japan? To get a glimpse, click on the following link of the “Design Your Waifu | Part Deux”, conducted by Naksatra and Shubham of 17MS!"
   },
   {
-    name: "REACT WORKSHOP",
+    name: "BASICS OF FIGMA",
     image: "./utils/images/events/5.png",
     description:
-      "React ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea     commodo consequat.Lorem ipsum dolor sit amet, consectetur",
+      "Learn how to make amazing posters, beautiful webpages, and more using Figma, a free, simple and easy to use online tool. Through the session, Ritobroto Mohanta took us through the toolset, the design principles one must follow, and how to translate great ideas into concrete designs."
   },
+  {
+    name: "LATEX WORKSHOP",
+    image: "./utils/images/events/5.png",
+    description:
+      "The workshop was conducted by Rohit Verma and Soumyajit Datta of 20MS, to provide a basic understanding of the tools, tips, and tricks that are needed to create your own LaTeX document. Once you learn LaTeX, you can make your assignments, lab reports, CVs, research papers and even your love letters stand out and look professional!"
+  },
+  {
+    name: "SLASHDOT TUTORIAL WELEARNBOT",
+    image: "./utils/images/events/5.png",
+    description:
+      "A small tutorial on how to use WelearnBot by Abhay Kshirsagar and Parth Bibekar <br> SlashDot unleashes mastering the mysteries in the coding world, through WeLearnBot, a Command-line tool for interacting with the WeLearn portal without the need for logging in through the browser. Click on the following link for an overview of the session conducted by Abhay Kshirsagar and Parth Bibekar of 19MS!"
+  }
 ];
 
-const upcomingEvents = [
-  {
-    name: "Slashdot - Alumni Talk",
-    date: "17th Jan 2022",
-    venue: "Google Meet",
-  },
-  {
-    name: "Cyber security and cyber crime awareness session",
-    date: "18th Jan 2022",
-    venue: "Zoom",
-  },
-  {
-    name: "gr security and cyber crime awareness session",
-    date: "18th Jan 2022",
-    venue: "aoom",
-  },
-  {
-    name: "qw security and cyber crime awareness session",
-    date: "20th Jan 2022",
-    venue: "boom",
-  },
-];
+const upcomingEvents = [];
+// const upcomingEvents = [
+//   {
+//     name: "Slashdot - Alumni Talk",
+//     date: "17th Jan 2022",
+//     venue: "Google Meet",
+//   },
+//   {
+//     name: "Cyber security and cyber crime awareness session",
+//     date: "18th Jan 2022",
+//     venue: "Zoom",
+//   },
+//   {
+//     name: "gr security and cyber crime awareness session",
+//     date: "18th Jan 2022",
+//     venue: "aoom",
+//   },
+//   {
+//     name: "qw security and cyber crime awareness session",
+//     date: "20th Jan 2022",
+//     venue: "boom",
+//   },
+// ];
 
 export { pastEvents, upcomingEvents };
