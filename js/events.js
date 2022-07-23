8b448f2f62e7d252cdbb55202a8b3d75a178e15a
AK6263 <abhaykshirsagar01@gmail.com>
2022-07-23 11:28:46 +0530

    added coming soon msg

--- js/events.js
+++ js/events.js
@@ -18,14 +18,19 @@ pastEventsUpdater();
 
 function upcomingEventsUpdater() {
   let eventBoxes = document.querySelectorAll(".upcoming-event-boxes")[0];
+  if(upcomingEvents.length == 0) {
+    const newElement = document.createElement('div');
+    newElement.innerHTML = `<div class="coming-soon">COMING SOON</div>`;
+    eventBoxes.insertAdjacentElement('beforeend', newElement);
+  }
   for (let index = 0; index < upcomingEvents.length; index++) {
     const newEvent = `<p class="upcoming-event-topic">${upcomingEvents[index].name}</p>
     <div class="event-line"></div>
     <p class="next-session">${upcomingEvents[index].date}</p>
     <p class="venue">${upcomingEvents[index].venue}</p>`;
-  const newElement = document.createElement('div');
-  newElement.classList.add('upcoming-event-box');
-  newElement.innerHTML = newEvent;
+    const newElement = document.createElement('div');
+    newElement.classList.add('upcoming-event-box');
+    newElement.innerHTML = newEvent;
     eventBoxes.insertAdjacentElement('beforeend', newElement);
   }
 }
