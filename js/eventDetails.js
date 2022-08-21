bc19e45f508c48aecb08eba009948430f2ae3b01
AK6263 <abhaykshirsagar01@gmail.com>
2022-08-21 19:31:58 +0530

    Added event images

--- js/eventDetails.js
+++ js/eventDetails.js
@@ -1,43 +1,43 @@
 const pastEvents = [
   {
     name: "WEBSITE DEVELOPMENT WORKSHOP",
-    image: "./utils/images/events/1.png",
+    image: "https://img.youtube.com/vi/nocmU2pfIO0/0.jpg",
     description: 
       "Presenting before you, the first ever milestone achieved by this club - the maiden workshop conducted, on Web Development skills. The audience spent an evening, surfing over the fundamentals of Git, GitHub and HTML Basics."
   },
   {
     name: "BEYOND ACADEMIA",
-    image: "./utils/images/events/2.png",
+    image: "https://img.youtube.com/vi//0.jpg",
     description:
       "Beyond Academia is a collaboration between Slashdot Coding and Design Club and IISER Kolkata Students' Alumni Cell, aiming to make you cognizant of the emerging computational tools used in scientific research.<br>In the very first episode of this series, to talk about Machine Learning, Data Science and Logics, we had with us, Dr. Sunipa Dev, an alumnus from 11MS DMS."
   },
   {
     name: "BLENDER WORKSHOP PART 1",
-    image: "./utils/images/events/3.png",
+    image: "https://img.youtube.com/vi/Tpm6PMod4q0/0.jpg",
     description:
       "The third workshop, presented by Pedisetti Venkatesh of 18MS, marked the inauguration of the Part 1 of Blender Basics, “Design Your Waifu” where we designed the ring that you would give to your waifu when she comes to laifu."
   },  
   {
     name: "BLENDER WORKSHOP PART 2",
-    image: "./utils/images/events/4.png",
+    image: "https://img.youtube.com/vi/b9KUQaB03ng/0.jpg",
     description:
       "Ladies and gentlemen, Part 2 of Design Your Waifu is here! Last time, we modelled and textured the ring that you'll be giving to your waifu when you finally create her. But how will you talk to her every day when she's all the way in Japan? To get a glimpse, click on the following link of the “Design Your Waifu | Part Deux”, conducted by Naksatra and Shubham of 17MS!"
   },
   {
     name: "BASICS OF FIGMA",
-    image: "./utils/images/events/5.png",
+    image: "https://img.youtube.com/vi/EPZmTm1rp7A/0.jpg",
     description:
       "Learn how to make amazing posters, beautiful webpages, and more using Figma, a free, simple and easy to use online tool. Through the session, Ritobroto Mohanta took us through the toolset, the design principles one must follow, and how to translate great ideas into concrete designs."
   },
   {
     name: "LATEX WORKSHOP",
-    image: "./utils/images/events/5.png",
+    image: "https://img.youtube.com/vi/K13G79Dl2no/0.jpg",
     description:
       "The workshop was conducted by Rohit Verma and Soumyajit Datta of 20MS, to provide a basic understanding of the tools, tips, and tricks that are needed to create your own LaTeX document. Once you learn LaTeX, you can make your assignments, lab reports, CVs, research papers and even your love letters stand out and look professional!"
   },
   {
     name: "SLASHDOT TUTORIAL WELEARNBOT",
-    image: "./utils/images/events/5.png",
+    image: "https://img.youtube.com/vi/hZfAOyDvK70/0.jpg",
     description:
       "A small tutorial on how to use WelearnBot by Abhay Kshirsagar and Parth Bibekar <br> SlashDot unleashes mastering the mysteries in the coding world, through WeLearnBot, a Command-line tool for interacting with the WeLearn portal without the need for logging in through the browser. Click on the following link for an overview of the session conducted by Abhay Kshirsagar and Parth Bibekar of 19MS!"
   }
